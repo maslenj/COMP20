adfcbba0bfdd21345741f870d2af1f3127d47d24
Jimmy Maslen <james.maslen@tufts.edu>
2021-03-23 12:23:45 -0400

    added json assignment

---
+++ JSONSongs/songs.js
@@ -0,0 +1,63 @@
+data = `{ "songs": 
+    [
+    {
+        "title": "Symphony No. 5",
+        "artist": "Ludwig van Beethoven",
+        "genre": "Classical",
+        "year": 1808
+    },
+    {
+        "title": "Cant Hold Us",
+        "artist": "Macklemore &amp; Ryan Lewis",
+        "genre": "Pop",
+        "year": 2012
+    },
+    {
+        "title": "The Box",
+        "artist": "Roddy Ricch",
+        "genre": "Hip-Hop/Rap",
+        "year": 2019
+    },
+    {
+        "title": "10 Bands",
+        "artist": "Drake",
+        "genre": "Hip-Hop/Rap",
+        "year": 2015
+    },
+    {
+        "title": "Come Get Her",
+        "artist": "Rae Sremmurd",
+        "genre": "Hip-Hop/Rap",
+        "year": 2015
+    },
+    {
+        "title": "Ray of Light",
+        "artist": "Madonna",
+        "genre": "Electronica",
+        "year": 1998
+    },
+    {
+        "title": "Old Town Road",
+        "artist": "Lil Nas X",
+        "genre": "Pop",
+        "year": 2019
+    },
+    {
+        "title": "The Boxer",
+        "artist": "Simon &amp; Garfunkel",
+        "genre": "Rock",
+        "year": 1965
+    },
+    {
+        "title": "Fast Car",
+        "artist": "Tracy Chapman",
+        "genre": "Rock",
+        "year": 1988
+    },
+    {
+        "title": "Hold On, Were Going Home",
+        "artist": "Drake, Majid Jordan",
+        "genre": "Hip-Hop/Rap",
+        "year": 2013
+    }
+]}`

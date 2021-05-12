f79befb3743df60f4e3dfa333e530c8e19c80527
Jimmy Maslen <james.maslen@tufts.edu>
2021-05-12 00:23:13 -0400

    add data

---
+++ assignments.js
@@ -0,0 +1,58 @@
+let assignments = 
+[
+  {
+    name: "Introduction",
+    number: 1,
+    description: "This project was intended to be an intrduction to basic html and css."
+  },
+  {
+    name: "Business Website",
+    number: 2,
+    description: "Fill in desc."
+  },
+  {
+    name: "XML Songs",
+    number: 3,
+    description: "Fill in desc."
+  },
+  {
+    name: "Joe's Hamburgers",
+    number: 4,
+    description: "Fill in desc."
+  },
+  {
+    name: "Lottery",
+    number: 5,
+    description: "Fill in desc."
+  },
+  {
+    name: "Ammicable Numbers",
+    number: 6,
+    description: "Fill in desc."
+  },
+  {
+    name: "Jade Delight 1",
+    number: 8,
+    description: "Fill in desc."
+  },
+  {
+    name: "JSON Songs",
+    number: 9,
+    description: "Fill in desc."
+  },
+  {
+    name: "API Fun",
+    number: 10,
+    description: "Fill in desc."
+  },
+  {
+    name: "Jade Delight 2",
+    number: 13,
+    description: "Fill in desc."
+  },
+  {
+    name: "Stock Ticker App",
+    number: 14,
+    description: "Fill in desc."
+  }
+]
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-fetch("./assignments.json")
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data));
+console.log(assignments)
+
+for (let assignment of assignments) {
+  document.getElementById("main-assignments").innerHTML +=
+    `
+      <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+          <img class="card-img-top" src="./images/assignment${assignment.number}.png"
+            alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Assignment ${assignment.number}: ${assignment.name}</h5>
+            <p class="card-text">${assignment.description}</p>
+            <a href="Assignment${assignment.number}" class="card-link">View assignment</a>
+            <a href="https://github.com/maslenj/COMP20/tree/master/Assignment${assignment.number}" class="card-link">View source code</a>
+          </div>
+        </div>
+      </div>
+  `
+}

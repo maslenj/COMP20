86823b7af55d9d50460bdcca11922a9eb0386357
Jimmy Maslen <maslenj4@gmail.com>
2021-05-26 15:20:42 -0700

    finish data formatting

--- assignments.js
+++ assignments.js
@@ -3,7 +3,7 @@
  *
 */
 
-let highlights =
+let assignments =
 [
   {
     name: "T-Shirt Hub",
@@ -14,7 +14,8 @@ let highlights =
     description: "This project is a mock T-Shirt website, complete with home, about, shop, \
     and contact pages. The project was done by a four-person team, but I was responsible for all \
     the shop pages and checkout modal.",
-    tags: ["HTML", "CSS", "JavaScript"]
+    tags: ["HTML", "CSS", "JavaScript"],
+    hilighted: true,
   },
   {
     name: "Crypto Tracker",
@@ -26,12 +27,9 @@ let highlights =
     crytocurrencies at their real values and sell currencies that they no longer want in their \
     portfolio. This project was a group project involving 4 group members, but I was involved in \
     building the crypto results, profile, and home pages, as well as many of the backend routes.",
-    tags: ["HTML", "CSS", "JavaScript", "JQuery", "Bootstrap", "NodeJS", "Express", "MongoDB"]
+    tags: ["HTML", "CSS", "JavaScript", "JQuery", "Bootstrap", "NodeJS", "Express", "MongoDB"],
+    hilighted: true,
   },
-]
-
-let assignments = 
-[
   {
     name: "Introduction",
     assignment: "Assignment 1",
@@ -40,7 +38,8 @@ let assignments =
     source: "https://github.com/maslenj/COMP20/tree/master/Assignment1",
     description: "This project was intended to be an intrduction to basic html and css. \
     I have included basic information about myself as well as some simple styling.",
-    tags: ["HTML", "CSS"]
+    tags: ["HTML", "CSS"],
+    hilighted: false,
   },
   {
     name: "Business Website",
@@ -50,72 +49,109 @@ let assignments =
     source: "https://github.com/maslenj/COMP20/tree/master/Assignment2",
     description: "A more advanced project involving just HTML and CSS. The site is designed for \
     a mock sandwich restaurant. I collaborated with one other student to build this project.",
-    tags: ["HTML", "CSS"]
+    tags: ["HTML", "CSS"],
+    hilighted: false,
   },
   {
     name: "XML Songs",
+    assignment: "Assignment 3",
     number: 3,
+    link: "Assignment3/songs.xml",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment3",
     description: "The purpose of this project was to organize data using XML and display it nicely \
     using CSS. I created an XML list of songs as well as relevant information about the songs and diplayed \
     it nicely with CSS.",
-    tags: ["XML", "CSS"]
+    tags: ["XML", "CSS"],
+    hilighted: false,
   },
   {
     name: "Joe's Hamburgers",
+    assignment: "Assignment 4",
     number: 4,
+    link: "Assignment4",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment4",
     description: "Introductory JavaScript assignment designed to take input from a \
     user and display relevant price information about certain foods.",
-    tags: ["JavaScript"]
+    tags: ["JavaScript"],
+    hilighted: false,
   },
   {
     name: "Lottery",
+    assignment: "Assignment 5",
     number: 5,
+    link: "Assignment5",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment5",
     description: "Mock lottery game in which users input 5 normal lotery numbers and \
     one 'lucky ball' and get to see the results of their picks based on a prize table.",
-    tags: ["JavaScript"]
+    tags: ["JavaScript"],
+    hilighted: false,
   },
   {
     name: "Ammicable Numbers",
+    assignment: "Assignment 6",
     number: 6,
+    link: "Assignment6",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment6",
     description: "Amicable numbers are numbers such that the sum of the proper \
     divisors of each number add up to the other number. In this project, users input \
     two numbers into the site and will see whether the numbers are amicable.",
-    tags: ["JavaScript"]
+    tags: ["JavaScript"],
+    hilighted: false,
   },
   {
     name: "Jade Delight 1",
+    assignment: "Assignment 8",
     number: 8,
+    link: "Assignment8",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment8",
     description: "In this project, users are given a form to fill out about what food they \
     would like to order. The form is then verified and parsed using JQuery, and results are \
     displayed.",
-    tags: ["JavaScript", "JQuery"]
+    tags: ["JavaScript", "JQuery"],
+    hilighted: false,
   },
   {
     name: "JSON Songs",
+    assignment: "Assignment 9",
     number: 9,
+    link: "Assignment9",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment9",
     description: "Similar to assignment 3, this project was to organize data using JSON \
     and display it nicely using JavaScript and CSS.",
-    tags: ["JSON", "JavaScript", "CSS"]
+    tags: ["JSON", "JavaScript", "CSS"],
+    hilighted: false,
   },
   {
     name: "API Fun",
+    assignment: "Assignment 10",
     number: 10,
+    link: "Assignment10",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment10",
     description: "Wrote XMLHTTP calls to <a href='https://swapi.dev/'> SWAPI</a> and displayed \
     data on page.",
-    tags: ["AJAX", "JavaScript"]
+    tags: ["AJAX", "JavaScript"],
+    hilighted: false,
   },
   {
     name: "Jade Delight 2",
+    assignment: "Assignment 13",
     number: 13,
+    link: "Assignment13",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment13",
     description: "This project is similar assignment 8, except instead of the form being parsed \
     by JQuery, the form is submitted to a PHP page and parsed there.",
-    tags: ["JavaScript", "PhP"]
+    tags: ["JavaScript", "PhP"],
+    hilighted: false,
   },
   {
     name: "Stock Ticker App",
+    assignment: "Assignment 14",
     number: 14,
+    link: "Assignment14",
+    source: "https://github.com/maslenj/COMP20/tree/master/Assignment14",
     description: "Web app designed to take form results and return either a stock ticker or company name \
     depending on input.",
-    tags: ["NodeJS", "Heroku"]
+    tags: ["NodeJS", "Heroku"],
+    hilighted: false,
   }
 ]
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
- * TODO: Change to only one for-loop
- *
- */
-
 const renderBadges = (tags) => {
   let res = " "
   for (let tag of tags) {
@@ -11,40 +6,24 @@ const renderBadges = (tags) => {
   return res
 }
 
-for (let assignment of assignments) {
-  document.getElementById("main-assignments").innerHTML +=
-    `
-      <div class="col-md-4 mb-3">
-        <div class="card mb-6 shadow-sm">
-          <img class="card-img-top" src="./images/assignment${assignment.number}.png"
-            alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Assignment ${assignment.number}: ${assignment.name}</h5>
-            <p class="card-text">${assignment.description}</p>
-            <p class="card-text">${renderBadges(assignment.tags)}</p>
-            <a href="Assignment${assignment.number}" class="card-link">View assignment</a>
-            <a href="https://github.com/maslenj/COMP20/tree/master/Assignment${assignment.number}" class="card-link">View source code</a>
-          </div>
-        </div>
-      </div>
-  `
-}
+const highlightContainer = document.getElementById("highlighted-assignments")
+const assignmentContainer = document.getElementById("main-assignments")
 
-for (let highlight of highlights) {
-  document.getElementById("highlighted-assignments").innerHTML +=
+for (let assignment of assignments) {
+  (assignment.hilighted ? highlightContainer : assignmentContainer).innerHTML +=
     `
       <div class="col-md-6 mb-3">
         <div class="card shadow-sm">
-          <img class="card-img-top" src="./images/assignment${highlight.number}.png"
+          <img class="card-img-top" src="./images/assignment${assignment.number}.png"
             alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">${highlight.assignment}: ${highlight.name}</h5>
-            <p class="card-text">${highlight.description}</p>
-            <p class="card-text">${renderBadges(highlight.tags)}</p>
-            <a href="${highlight.link}" class="card-link">View assignment</a>
-            <a href="${highlight.source}" class="card-link">View source code</a>
+            <h5 class="card-title">${assignment.assignment}: ${assignment.name}</h5>
+            <p class="card-text">${assignment.description}</p>
+            <p class="card-text">${renderBadges(assignment.tags)}</p>
+            <a href="${assignment.link}" class="card-link">View assignment</a>
+            <a href="${assignment.source}" class="card-link">View source code</a>
           </div>
         </div>
       </div>
-  `
+    `
 }
